31e7e7511d5e5abe9b2874d1c8d4985384d65301
Efrain Lemus <efraintlemusm@gmail.com>
2023-08-14 10:54:30 -0600

    add style variations

--- plugins/multi-columns/build/index.asset.php
+++ plugins/multi-columns/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'e5def5677e8801a14eb3');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'aea9b4a6d1f8f7d6ccf3');
